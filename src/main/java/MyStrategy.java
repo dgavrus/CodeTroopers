e43a6686a1c9111ddfe8537cfc6f522d4847eb1a
iksuy <denis.gavrus@gmail.com>
2013-11-07 17:23:12 +0300

    Changes in bfs, added parameter considerTeammateAsObstacle Now medic can heals injury teammates which stay for some distance HashMap pointsToMove added

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -10,8 +10,12 @@ import java.util.Random;
 
 public final class MyStrategy implements Strategy {
     private final Random random = new Random();
-    private static ArrayList<TrooperType> start = new ArrayList<TrooperType>();
-    private static Point pointToAttack;
+    private static HashMap<TrooperType, Point> pointsToMove = new HashMap<TrooperType, Point>();
+    private static Point globalPointToAttack = null;
+    private static Point medicMovePoint;
+    private static Point soldierMovePoint;
+    private static Point commanderMovePoint;
+    
     enum Area {LEFT_BOTTOM, LEFT_TOP, RIGHT_BOTTOM, RIGHT_TOP};
     
     @Override
@@ -19,27 +23,57 @@ public final class MyStrategy implements Strategy {
         if (self.getActionPoints() < game.getStandingMoveCost()) {
             return;
         }
-        removeDeadTrooper(world);
         Trooper enemy = getEnemy(self, world);
+        //UseMedikit for each other
         if(self.getHitpoints() <= self.getMaximalHitpoints() / 2 &&
         		self.isHoldingMedikit() && self.getActionPoints() >= game.getMedikitUseCost()) {
         	move.setAction(ActionType.USE_MEDIKIT);
         	move.setDirection(Direction.CURRENT_POINT);
-        } else if(self.getType() == TrooperType.FIELD_MEDIC && 
+        }
+        //Wait for a medic
+        else if(self.getHitpoints() < self.getMaximalHitpoints() && 
+        		enemy == null && 
+        		self.getType() != TrooperType.FIELD_MEDIC &&
+        		getMyMedic(world) != null && getMyMedic(world).getHitpoints() > 0){
+        	move.setAction(ActionType.END_TURN);
+        	move.setDirection(Direction.CURRENT_POINT);
+        }
+        //Heal teammate if he is near the medic
+        else if(self.getType() == TrooperType.FIELD_MEDIC && 
         	getInjuryTeammateNear(self, world) != null &&
         	self.getActionPoints() >= game.getFieldMedicHealCost()){
         	move.setAction(ActionType.HEAL);
         	move.setX(getInjuryTeammateNear(self, world).getX());
         	move.setY(getInjuryTeammateNear(self, world).getY());
         	System.out.println("HEAL");
-        } else if(self.getHitpoints() <= self.getMaximalHitpoints() / 2 &&
+        }
+        //Go to the injured teammate
+        else if(self.getType() == TrooperType.FIELD_MEDIC &&
+        		getInjuryTeammate(self, world) != null &&
+        		self.getActionPoints() >= game.getStandingMoveCost() && 
+        		(enemy == null || 
+        		(enemy != null && 
+        		!world.isVisible(self.getShootingRange(), self.getX(), self.getY(), 
+        				self.getStance(), enemy.getX(), enemy.getY(), enemy.getStance())))){
+        	setPointToMove(self, new Point(getInjuryTeammate(self, world).getX(), getInjuryTeammate(self, world).getY()));
+        	move.setAction(ActionType.MOVE);
+        	move.setDirection(bfs(medicMovePoint.x, medicMovePoint.y, world, self, true));
+        }
+        //Medic heal himself
+        else if(self.getHitpoints() < 2.0 * self.getMaximalHitpoints() / 3 &&
         		self.getType() == TrooperType.FIELD_MEDIC &&
-        		self.getActionPoints() >= game.getFieldMedicHealCost()){
+        		self.getActionPoints() >= game.getFieldMedicHealCost() &&
+        		(enemy == null || 
+                (enemy != null && 
+                !world.isVisible(self.getShootingRange(), self.getX(), self.getY(), 
+                		self.getStance(), enemy.getX(), enemy.getY(), enemy.getStance())))){
         	move.setAction(ActionType.HEAL);
         	move.setDirection(Direction.CURRENT_POINT);
         } else if(enemy != null){
         	attackEnemy(self, enemy, game, move);
-        } else moveTo(self, world, move);
+        } else if(self.getActionPoints() >= game.getStandingMoveCost()){
+        	moveTo(self, world, move);
+        }
     }
     
     private Trooper getEnemy(Trooper self, World world){
@@ -62,6 +96,11 @@ public final class MyStrategy implements Strategy {
     }
     
     private void attackEnemy(Trooper self, Trooper enemy, Game game, Move move){
+    	if(self.isHoldingFieldRation()){
+    		move.setAction(ActionType.EAT_FIELD_RATION);
+    		move.setDirection(Direction.CURRENT_POINT);
+    		return;
+    	}
     	if(self.isHoldingGrenade() && 
     			self.getActionPoints() >= game.getGrenadeThrowCost() &&
     			self.getDistanceTo(enemy) <= game.getGrenadeThrowRange()){
@@ -75,83 +114,125 @@ public final class MyStrategy implements Strategy {
     }
     
     private void moveTo(Trooper self, World world, Move move){
-    	if(!start.contains(self.getType())){
-    		start.add(self.getType());
-    		Area myArea = getMyArea(self, world);
-        	pointToAttack = getPointToAttack(myArea, world);
-        	System.out.println("HI1");
+    	if(globalPointToAttack == null){
+    		globalPointToAttack = getGlobalPointToAttack(getMyArea(self, world), world);
     	}
-    	if(self.getX() == pointToAttack.x &&
-        		self.getY() == pointToAttack.y){
-    		System.out.println("HI2");
-    		start.clear();
+    	if(!pointsToMove.containsKey(self.getType())){
+    		pointsToMove.put(self.getType(), globalPointToAttack);
     	}
-    	Direction direction = bfs(pointToAttack.x, pointToAttack.y, world, self);
+    	if(self.getX() == pointsToMove.get(self.getType()).x &&
+        		self.getY() == pointsToMove.get(self.getType()).y){
+    		if(globalPointToAttack.equals(new Point(self.getX(), self.getY()))){
+    			globalPointToAttack = getGlobalPointToAttack(getMyArea(self, world), world);
+    		}
+    		pointsToMove.put(self.getType(), globalPointToAttack);
+    	}
+    	Direction direction = bfs(
+    			pointsToMove.get(self.getType()).x, pointsToMove.get(self.getType()).y, world, self, true);
     	move.setAction(ActionType.MOVE);
     	move.setDirection(direction);
     }
     
-    private Direction bfs(int x, int y, World world, Trooper self){
-    	Queue<Point> q = new ArrayDeque<Point>();
-    	q.add(new Point(self.getX(), self.getY()));
-    	boolean[][] used = new boolean[world.getWidth()][world.getHeight()];
-    	int[][] d = new int[world.getWidth()][world.getHeight()];
-    	Point[][] p = new Point[world.getWidth()][world.getHeight()];
-    	used[self.getX()][self.getY()] = true;
-    	p[self.getX()][self.getY()] = new Point(-1, -1);
-    	boolean isWayFound = false;
-    	while (!q.isEmpty() && !isWayFound) {
-    		Point currentPoint = q.poll();
-    		if(used[x][y]){
+    private void setPointToMove(Trooper self, Point point){
+    	switch(self.getType()){
+    		case FIELD_MEDIC:
+    			medicMovePoint = point;
     			break;
-    		}
-			for(int xx = currentPoint.x - 1; xx <= currentPoint.x + 1; xx += 2){
-					if(isValidFreeCell(world, xx, currentPoint.y) && !used[xx][currentPoint.y]){
-						used[xx][currentPoint.y] = true;
-						q.add(new Point(xx, currentPoint.y));
-						d[xx][currentPoint.y] = d[currentPoint.x][currentPoint.y] + 1;
-						p[xx][currentPoint.y] = currentPoint; 
-					}
-			}
-			for(int yy = currentPoint.y - 1; yy <= currentPoint.y + 1; yy += 2){
-				if(isValidFreeCell(world, currentPoint.x, yy) && !used[currentPoint.x][yy]){
-					used[currentPoint.x][yy] = true;
-					q.add(new Point(currentPoint.x, yy));
-					d[currentPoint.x][yy] = d[currentPoint.x][currentPoint.y] + 1;
-					p[currentPoint.x][yy] = currentPoint; 
-				}
-			}	
-    	}
-    	Point currentPoint = null;
-    	Point prevPoint = new Point(x, y);
-    	do {
-    		prevPoint = (currentPoint != null ?
-    				currentPoint : new Point(x, y));
-    		currentPoint = (
-    				 currentPoint != null ?
-    				 p[currentPoint.x][currentPoint.y] : 
-    				 new Point(x, y)
-    				 ); 
-    	} while(!p[currentPoint.x][currentPoint.y].equals(new Point(-1, -1)));
-    	
-    	if(prevPoint.x == self.getX() + 1){
-    		return Direction.EAST;
-    	} else if (prevPoint.x == self.getX() - 1){
-    		return Direction.WEST;
-    	} else if(prevPoint.y == self.getY() + 1){
-    		return Direction.SOUTH;
-    	} else {
-    		return Direction.NORTH;
+    		case COMMANDER:
+    			commanderMovePoint = point;
+    			break;
+    		case SOLDIER:
+    			soldierMovePoint = point;
+    			break;
     	}
     }
     
-	private boolean isValidFreeCell(World world, int x, int y){
-		if(x >= 0 && x < world.getWidth() &&
-				y >= 0 && y < world.getHeight() &&
-				world.getCells()[x][y] == CellType.FREE){
-			return true;
+    private Direction bfs(int x, int y, World world, Trooper self, boolean considerTeammateAsObstacle){
+    	try {
+	    	Queue<Point> q = new ArrayDeque<Point>();
+	    	q.add(new Point(self.getX(), self.getY()));
+	    	boolean[][] used = new boolean[world.getWidth()][world.getHeight()];
+	    	int[][] d = new int[world.getWidth()][world.getHeight()];
+	    	Point[][] p = new Point[world.getWidth()][world.getHeight()];
+	    	used[self.getX()][self.getY()] = true;
+	    	p[self.getX()][self.getY()] = new Point(-1, -1);
+	    	boolean isWayFound = false;
+	    	while (!q.isEmpty() && !isWayFound) {
+	    		Point currentPoint = q.poll();
+	    		if(used[x][y]){
+	    			break;
+	    		}
+				for(int xx = currentPoint.x - 1; xx <= currentPoint.x + 1; xx += 2){
+						if(isValidFreeCell(world, xx, currentPoint.y, 
+								new Point(x, y), considerTeammateAsObstacle) && 
+								!used[xx][currentPoint.y]){
+							used[xx][currentPoint.y] = true;
+							q.add(new Point(xx, currentPoint.y));
+							d[xx][currentPoint.y] = d[currentPoint.x][currentPoint.y] + 1;
+							p[xx][currentPoint.y] = currentPoint; 
+						}
+				}
+				for(int yy = currentPoint.y - 1; yy <= currentPoint.y + 1; yy += 2){
+					if(isValidFreeCell(world, currentPoint.x, yy, 
+							new Point(x, y), considerTeammateAsObstacle) && 
+						!used[currentPoint.x][yy]){
+						used[currentPoint.x][yy] = true;
+						q.add(new Point(currentPoint.x, yy));
+						d[currentPoint.x][yy] = d[currentPoint.x][currentPoint.y] + 1;
+						p[currentPoint.x][yy] = currentPoint; 
+					}
+				}	
+	    	}
+	    	if(!used[x][y]){
+	    		return bfs(x, y, world, self, false);
+	    	}
+	    	Point currentPoint = null;
+	    	Point prevPoint = new Point(x, y);
+	    	do {
+	    		prevPoint = (currentPoint != null ?
+	    				currentPoint : new Point(x, y));
+	    		currentPoint = (
+	    				 currentPoint != null ?
+	    				 p[currentPoint.x][currentPoint.y] : 
+	    				 new Point(x, y)
+	    				 ); 
+	    	} while(!p[currentPoint.x][currentPoint.y].equals(new Point(-1, -1)));
+	    	
+	    	if(prevPoint.x == self.getX() + 1){
+	    		return Direction.EAST;
+	    	} else if (prevPoint.x == self.getX() - 1){
+	    		return Direction.WEST;
+	    	} else if(prevPoint.y == self.getY() + 1){
+	    		return Direction.SOUTH;
+	    	} else {
+	    		return Direction.NORTH;
+	    	}
+    	} catch (NullPointerException e){
+    		return bfs(x, y, world, self, false);
+    	}
+    }
+    
+	private boolean isValidFreeCell(
+			World world, int x, int y, 
+			Point dest, boolean considerTeammateAsObstacle
+			){
+		if(considerTeammateAsObstacle){
+			if(x >= 0 && x < world.getWidth() &&
+					y >= 0 && y < world.getHeight() &&
+					world.getCells()[x][y] == CellType.FREE && 
+					!isTeammateTrooperInCell(new Point(x, y), world, dest)){
+				return true;
+			}
+			return false;
+		} else {
+			if(x >= 0 && x < world.getWidth() &&
+					y >= 0 && y < world.getHeight() &&
+					world.getCells()[x][y] == CellType.FREE){
+				return true;
+			}
+			return false;
 		}
-		return false;
+		
 	}
 
 	private Area getMyArea(Trooper self, World world){
@@ -166,7 +247,7 @@ public final class MyStrategy implements Strategy {
 			return Area.RIGHT_BOTTOM;
 	}
 	
-	private Point getPointToAttack(Area area, World world){
+	private Point getGlobalPointToAttack(Area area, World world){
 		if(area == Area.LEFT_BOTTOM){
 			return random.nextBoolean() ? 
 					new Point(0, 0) : 
@@ -182,31 +263,54 @@ public final class MyStrategy implements Strategy {
 		} else 
 			return random.nextBoolean() ? 
 					new Point(world.getWidth() - 1, 0) :
-					new Point(0, 0);
-	}
-	
-	private void removeDeadTrooper(World world){
-		for(Trooper trooper : world.getTroopers()){
-			if(trooper.isTeammate() && 
-					start.contains(trooper.getType()) &&
-					trooper.getHitpoints() == 0){
-				start.remove(trooper);
-			}
-		}
+					new Point(0, world.getHeight() - 1);
 	}
 	
 	private Trooper getInjuryTeammateNear(Trooper self, World world){
 		for(Trooper trooper : world.getTroopers()){
 			if(trooper.isTeammate() && 
 					self.getDistanceTo(trooper) == 1 && 
-					trooper.getHitpoints() < trooper.getMaximalHitpoints()){
-				System.out.println(trooper.getType());
+					trooper.getHitpoints() < trooper.getMaximalHitpoints() &&
+					trooper.getHitpoints() > 0){
 				return trooper;
 			}
 		}
 		return null;
 	}
 	
+	private Trooper getInjuryTeammate(Trooper self, World world){
+		for(Trooper trooper : world.getTroopers()){
+			if(trooper.isTeammate() && 
+					self.getDistanceTo(trooper) > 1 && 
+					trooper.getHitpoints() < trooper.getMaximalHitpoints() &&
+					trooper.getHitpoints() > 0){
+				return trooper;
+			}
+		}
+		return null;
+	}
+
+	private Trooper getMyMedic(World world){
+		for(Trooper trooper : world.getTroopers()){
+			if(trooper.isTeammate() &&
+					trooper.getType() == TrooperType.FIELD_MEDIC){
+				return trooper;
+			}
+		}
+		return null;
+	}
+
+	private boolean isTeammateTrooperInCell(Point point, World world, Point dest){
+		for(Trooper trooper : world.getTroopers()){
+			if(trooper.isTeammate() && 
+					trooper.getX() == point.x &&
+					trooper.getY() == point.y &&
+					!point.equals(dest)){
+				return true;
+			}
+		}
+		return false;
+	}
 }
 
 
